7adec34224800781df4b584c6658094a2d421b7e
David Lezcano Becerra <davidbeceralezcano@gmail.com>
2023-02-16 02:13:23 -0500

    detalles

---
+++ public/navbar.js
@@ -0,0 +1,8 @@
+const flipTranslate = () => {
+    document.getElementById('navDTM').classList.toggle('translatex')
+    document.getElementById('search').classList.toggle('translateIx')
+    document.getElementById('name').classList.toggle('translateIx')
+    document.getElementById('close').classList.toggle('dNone')
+    document.getElementById('close').classList.toggle('translatex')
+    document.getElementById('open').classList.toggle('translatex')
+}
